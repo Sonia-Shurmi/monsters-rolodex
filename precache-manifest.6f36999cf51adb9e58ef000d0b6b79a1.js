59ad3fae7f961b5f771e5cfb16136f9b05a36567
Sonia Shurmi <soniashurmi@gmail.com>
2019-09-15 13:02:46 +0600

    Updates

---
+++ precache-manifest.6f36999cf51adb9e58ef000d0b6b79a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411e626b8412fc3a84558a1b49d54bd4",
+    "url": "/Sonia-Shurmi/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f495678c866dbc2fcbaa",
+    "url": "/Sonia-Shurmi/monsters-rolodex/static/css/main.4ce1f0e1.chunk.css"
+  },
+  {
+    "revision": "e260c628452a8e3fa795",
+    "url": "/Sonia-Shurmi/monsters-rolodex/static/js/2.cdcd8c14.chunk.js"
+  },
+  {
+    "revision": "f495678c866dbc2fcbaa",
+    "url": "/Sonia-Shurmi/monsters-rolodex/static/js/main.a3bc1216.chunk.js"
+  },
+  {
+    "revision": "57d28b3a47e1fcb6d55d",
+    "url": "/Sonia-Shurmi/monsters-rolodex/static/js/runtime~main.c9335bec.js"
+  }
+]);
